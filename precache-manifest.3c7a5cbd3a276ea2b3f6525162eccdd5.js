6b7c51819e5413e960c4fa22c465750a3d781d2d
jgabriel.tamay@gmail.com <jgabriel.tamay@gmail.com>
2020-07-19 15:00:14 -0300

    Updates

---
+++ precache-manifest.3c7a5cbd3a276ea2b3f6525162eccdd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ec8da1087845bd8a193675d05ff204",
+    "url": "/quiniela/index.html"
+  },
+  {
+    "revision": "54ebcf5b12cf0157785e",
+    "url": "/quiniela/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33e29614dfd41dbaca07",
+    "url": "/quiniela/static/js/2.545c5a2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiniela/static/js/2.545c5a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ebcf5b12cf0157785e",
+    "url": "/quiniela/static/js/main.cf425df6.chunk.js"
+  },
+  {
+    "revision": "9ba91447ecd0364d7a56",
+    "url": "/quiniela/static/js/runtime-main.6590d16d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiniela/static/media/logo.5d5d9eef.svg"
+  }
+]);
